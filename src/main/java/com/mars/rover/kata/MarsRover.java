39c76db5a2ae1d7f128a9e139f60ce429c27af3e
Juhita Chevli <juhitachevli@gmail.com>
2025-06-11 15:43:09 +0100

    add test for trying to move the rover with an invalid input and add pitest for coverage. add target to gitignore

--- src/main/java/com/mars/rover/kata/MarsRover.java
+++ src/main/java/com/mars/rover/kata/MarsRover.java
@@ -2,18 +2,16 @@ package com.mars.rover.kata;
 
 import java.util.stream.IntStream;
 
-public class MarsRover
-{
-    private String direction;
-    private int[] position;
+public class MarsRover {
+    private final String direction;
+    private final int[] position;
 
     public MarsRover(String direction, int[] position) {
         this.direction = direction;
         this.position = position;
     }
 
-    public String getDirection()
-    {
+    public String getDirection() {
         return direction;
     }
 
@@ -21,17 +19,19 @@ public class MarsRover
         return position;
     }
 
-    public int[] moveForward(char sequence)
-    {
-        if (direction.equals("N"))
-        {
-            return IntStream.range(0, position.length).map(i -> i==1 ? position[i]+1:position[i]).toArray();
-        } else if (direction.equals("E")){
-            return IntStream.range(0, position.length).map(i -> i==0 ? position[i]+1:position[i]).toArray();
-        } else if (direction.equals("S")){
-            return IntStream.range(0, position.length).map(i -> i==1 ? position[i]-1:position[i]).toArray();
-        } else if (direction.equals("W")){
-            return IntStream.range(0, position.length).map(i -> i==0 ? position[i]-1:position[i]).toArray();
+    public int[] moveForward(char sequence) {
+        if (sequence == 'F') {
+            switch (direction) {
+                case "N":
+                    return IntStream.range(0, position.length).map(i -> i == 1 ? position[i] + 1 : position[i]).toArray();
+                case "E":
+                    return IntStream.range(0, position.length).map(i -> i == 0 ? position[i] + 1 : position[i]).toArray();
+                case "S":
+                    return IntStream.range(0, position.length).map(i -> i == 1 ? position[i] - 1 : position[i]).toArray();
+                case "W":
+                    return IntStream.range(0, position.length).map(i -> i == 0 ? position[i] - 1 : position[i]).toArray();
+            }
+            return position;
         }
         return position;
     }
--- src/test/java/com/mars/rover/kata/MarsRoverKataTest.java
+++ src/test/java/com/mars/rover/kata/MarsRoverKataTest.java
@@ -5,48 +5,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class MarsRoverKataTest
-{
+public class MarsRoverKataTest {
     @Test
-    void getRoverDirection()
-    {
-        MarsRover marsRover = new MarsRover("N", new int[] {1,1});
+    void getRoverDirection() {
+        MarsRover marsRover = new MarsRover("N", new int[]{1, 1});
         assertEquals("N", marsRover.getDirection());
     }
 
     @Test
-    void getRoverPosition()
-    {
-        MarsRover marsRover = new MarsRover("N", new int[] {1,1});
+    void getRoverPosition() {
+        MarsRover marsRover = new MarsRover("N", new int[]{1, 1});
         assertArrayEquals(new int[]{1, 1}, marsRover.getPosition());
     }
 
     @Test
-    void moveRoverForwardWhenFacingNorth()
-    {
-        MarsRover marsRover = new MarsRover("N", new int[]{1,1});
-        assertArrayEquals(new int[]{1,2}, marsRover.moveForward('F'));
+    void moveRoverForwardWhenFacingNorth() {
+        MarsRover marsRover = new MarsRover("N", new int[]{1, 1});
+        assertArrayEquals(new int[]{1, 2}, marsRover.moveForward('F'));
     }
 
     @Test
-    void moveRoverForwardWhenFacingEast()
-    {
-        MarsRover marsRover = new MarsRover("E", new int[]{1,1});
-        assertArrayEquals(new int[]{2,1}, marsRover.moveForward('F'));
+    void moveRoverForwardWhenFacingEast() {
+        MarsRover marsRover = new MarsRover("E", new int[]{1, 1});
+        assertArrayEquals(new int[]{2, 1}, marsRover.moveForward('F'));
     }
 
     @Test
-    void moveRoverForwardWhenFacingSouth()
-    {
-        MarsRover marsRover = new MarsRover("S", new int[]{1,1});
-        assertArrayEquals(new int[]{1,0}, marsRover.moveForward('F'));
+    void moveRoverForwardWhenFacingSouth() {
+        MarsRover marsRover = new MarsRover("S", new int[]{1, 1});
+        assertArrayEquals(new int[]{1, 0}, marsRover.moveForward('F'));
     }
 
     @Test
-    void moveRoverFowardWhenFacingWest()
-    {
-        MarsRover marsRover = new MarsRover("W", new int[]{1,1});
-        assertArrayEquals(new int[]{0,1}, marsRover.moveForward('F'));
+    void moveRoverFowardWhenFacingWest() {
+        MarsRover marsRover = new MarsRover("W", new int[]{1, 1});
+        assertArrayEquals(new int[]{0, 1}, marsRover.moveForward('F'));
     }
 
+    @Test
+    void tryToMoveRoverFowardWithInvalidInput() {
+        MarsRover marsRover = new MarsRover("I", new int[]{1, 1});
+        assertArrayEquals(new int[]{1, 1}, marsRover.moveForward('F'));
+    }
+
+    @Test
+    void tryToMoveRoverWithInvalidInput() {
+        MarsRover marsRover = new MarsRover("N", new int[]{1, 1});
+        assertArrayEquals(new int[]{1, 1}, marsRover.moveForward('I'));
+    }
 }
